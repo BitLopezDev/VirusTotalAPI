074ee163e81877a8c66d6fcfc168809da7a08707
Santiago C <3glbbv5pg@mozmail.com>
2023-05-30 12:14:55 -0300

    basic form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,51 @@
+'use client'
 import RootLayout from './layout';
 import { TheHead } from '../components/headf';
+import React from 'react';
+//var $ = require("jquery");
+
+
 
 export default function Home() {
+
+  const initialFormData = Object.freeze({
+    url: "www.google.com",
+
+  });
+
+  const [formData, updateFormData]: any = React.useState(initialFormData);
+  const handleChange = (e: any) => {
+
+    updateFormData({
+      ...formData,
+
+      // Trimming any whitespace
+
+      [e.target?.name]: e.target.value.trim()
+    });
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    console.log(formData);
+    // ... submit to API or something
+
+    alert(JSON.stringify(formData.url, null, 4));
+  };
+  const objeto = {
+    data: 1,
+    dato: ''
+  }
   return (
     <>
       {/* <TheHead /> */}
 
       <RootLayout>
-        <p>Hello World</p>
+        <form>
+          <input type="text" name="url" placeholder="www.google.com" value="www.google.com" onChange={handleChange} />
+          <input type="submit" onClick={handleSubmit} />
+        </form>
+
       </RootLayout>
 
     </>
